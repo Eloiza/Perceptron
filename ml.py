c607d9104cd5d90ba2e6233b2a10e1d933d51076
Eloiza <eloiza.rossetto@gmail.com>
2021-02-01 18:29:30 -0300

    Retirando o classificar perceptron do sklearn

--- ml.py
+++ ml.py
@@ -26,91 +26,6 @@ def print_mc(matrix):
 		print(str(l)[1:-1]) 
 
 
-
-def plot_confusion_matrix(cm,
-                          target_names,
-                          title='Confusion matrix',
-                          cmap=None,
-                          normalize=True):
-    """
-    given a sklearn confusion matrix (cm), make a nice plot
-
-    Arguments
-    ---------
-    cm:           confusion matrix from sklearn.metrics.confusion_matrix
-
-    target_names: given classification classes such as [0, 1, 2]
-                  the class names, for example: ['high', 'medium', 'low']
-
-    title:        the text to display at the top of the matrix
-
-    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
-                  see http://matplotlib.org/examples/color/colormaps_reference.html
-                  plt.get_cmap('jet') or plt.cm.Blues
-
-    normalize:    If False, plot the raw numbers
-                  If True, plot the proportions
-
-    Usage
-    -----
-    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
-                                                              # sklearn.metrics.confusion_matrix
-                          normalize    = True,                # show proportions
-                          target_names = y_labels_vals,       # list of names of the classes
-                          title        = best_estimator_name) # title of graph
-
-    Citiation
-    ---------
-    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-
-    """
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import itertools
-
-    font = {'family' : 'normal',
-        'size'   : 14}
-
-    plt.rc('font', **font)
-
-    accuracy = np.trace(cm) / float(np.sum(cm))
-    misclass = 1 - accuracy
-
-    if cmap is None:
-        cmap = plt.get_cmap('Blues')
-
-    plt.figure(figsize=(8, 6))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-
-
-    if target_names is not None:
-        tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=45)
-        plt.yticks(tick_marks, target_names)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-
-    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if normalize:
-            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-        else:
-            plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-
-    plt.tight_layout()
-    plt.ylabel('Classe Original')
-    plt.xlabel('Classe Predita\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
-    plt.show()
-
 if __name__ == "__main__":
     DATASET_LIST = [
         "xor",
@@ -165,25 +80,18 @@ if __name__ == "__main__":
 
         mlp = MLPClassifier(random_state=1, max_iter=100).fit(trainX, trainY)
 
-        #perceptron do scikit-learn
-        perceptron_sk = Perceptron(random_state=13, max_iter=300).fit(trainX, trainY)
-
         print("Testando Modelos...")
         mlp_pred   = mlp.predict(testX)
         percp_pred = perceptron.predict(testX)
-        percp_pred_sk = perceptron_sk.predict(testX)
 
         mlp_acc   = accuracy_score(testY, mlp_pred)
         percp_acc = accuracy_score(testY, percp_pred) 
-        percp_acc_sk = accuracy_score(testY, percp_pred_sk) 
 
         mlp_f1    = f1_score(testY, mlp_pred)
         percpt_f1 = f1_score(testY, percp_pred)
-        percpt_f1_sk = f1_score(testY, percp_pred_sk)
 
         mlp_matrix    = confusion_matrix(testY, mlp_pred)
         percpt_matrix = confusion_matrix(testY, percp_pred)
-        percpt_matrix_sk = confusion_matrix(testY, percp_pred_sk)
 
 
         print("==========Perceptron==========")
@@ -192,24 +100,8 @@ if __name__ == "__main__":
         print("------Matriz de Confusão------")
         print_mc(percpt_matrix)
 
-        print("=========Perceptron SK=========")
-        print("Acurácia: %.2f" %(percp_acc_sk*100))
-        print("F1 Score: %.2f" %(percpt_f1_sk*100))
-        print("------Matriz de Confusão------")
-        print_mc(percpt_matrix_sk)
-
-
         print("==============MLP=============")
         print("Acurácia: %.2f" %(mlp_acc*100))
         print("F1 Score: %.2f" %(mlp_f1*100))
         print("------Matriz de Confusão------")
         print_mc(mlp_matrix)
-
-
-        if(DATASET_NAME == 'xor'):
-            plot_confusion_matrix(cm= mlp_matrix, target_names= ["0", "1"], title='Matriz de Confusão MLP - Base XOR', cmap=None, normalize=False)
-            plot_confusion_matrix(cm= percpt_matrix, target_names= ["0", "1"], title='Matriz de Confusão Perceptron - Base XOR', cmap=None, normalize=False)
-
-        else:
-            plot_confusion_matrix(cm= mlp_matrix, target_names= ["Ímpar", "par"], title='Matriz de Confusão MLP - Base MNIST', cmap=None, normalize=False)
-            plot_confusion_matrix(cm= percpt_matrix, target_names= ["Ímpar", "par"], title='Matriz de Confusão Perceptron - Base MNIST', cmap=None, normalize=False)
